20c4cc53edc168afd863fd9e5e37e0989d7a3a58
bhavadharanii <bhavadharan.s@fundsindia.com>
2022-12-22 18:24:23 +0530

    Basics of tab controller

---
+++ Tab bar/Tab bar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tab bar
+//
+//  Created by Fiuser on 21/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
